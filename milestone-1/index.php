da2423fbd3af39f93e283dba1a8a5417e5d98a1a
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-02-02 16:36:17 +0100

    Sostituiti i dati delle card
    Sostituiti i dati delle card

--- milestone-1/index.php
+++ milestone-1/index.php
@@ -11,40 +11,40 @@ Attraverso l’utilizzo di AJAX: al caricamento della pagina ajax chiederà attr
 
 $dischi = [
     ['<div class="card"> 
-        <img src="../dist/img/logo.png">
+        <img src="../dist/img/new_jersey.jpg">
         <h3>New Jersey</h3>
         <div>Bon Jovi</div>
         <div>1988</div>
     </div>'],
     ['<div class="card"> 
-        <img src="../dist/img/logo.png">
-        <h3>New Jersey</h3>
-        <div>Bon Jovi</div>
-        <div>1988</div>
+        <img src="../dist/img/queen.jpg">
+        <h3>Live at Wembley 86</h3>
+        <div>Queen</div>
+        <div>1992</div>
     </div>'],
     ['<div class="card"> 
-        <img src="../dist/img/logo.png">
-        <h3>New Jersey</h3>
-        <div>Bon Jovi</div>
-        <div>1988</div>
+        <img src="../dist/img/sting.jpg">
+        <h3>Ten\'s Summoner\'s Tales</h3>
+        <div>Sting</div>
+        <div>1993</div>
     </div>'],
     ['<div class="card"> 
-        <img src="../dist/img/logo.png">
-        <h3>New Jersey</h3>
-        <div>Bon Jovi</div>
-        <div>1988</div>
+        <img src="../dist/img/steve_gadd.jpg">
+        <h3>Steve Gadd Band</h3>
+        <div>Steve Gadd Band</div>
+        <div>2018</div>
     </div>'],
     ['<div class="card"> 
-        <img src="../dist/img/logo.png">
-        <h3>New Jersey</h3>
-        <div>Bon Jovi</div>
-        <div>1988</div>
+        <img src="../dist/img/ironmaiden.jpeg">
+        <h3>Brave new World</h3>
+        <div>Iron Maiden</div>
+        <div>2000</div>
     </div>'],
     ['<div class="card"> 
-        <img src="../dist/img/logo.png">
-        <h3>New Jersey</h3>
-        <div>Bon Jovi</div>
-        <div>1988</div>
+        <img src="../dist/img/eric_clapton.jpg">
+        <h3>One more car, one more rider</h3>
+        <div>Eric Clapton</div>
+        <div>2002</div>
     </div>'],
 ];
 
