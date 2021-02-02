aec07b6e49b607d3d45d327b5874636a060e1b27
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-02-02 19:49:40 +0100

    Dato uno stile alle cards
    Dato uno stile alle cards

--- milestone-1/index.php
+++ milestone-1/index.php
@@ -78,17 +78,19 @@ foreach ($dischi as $card) {
 
         <!-- main -->
         <main>
-            <!-- ciclo foreach per stampare le card -->
-            <?php foreach ($dischi as $card) { ?>
-                <!-- ciclo foreach per stampare i dati delle card -->
-                <?php foreach ($card as $datiCard) { ?>         
-                    <?php 
-                        echo $datiCard;
-                    ?>
+            <div class="container_cards">
+                <!-- ciclo foreach per stampare le card -->
+                <?php foreach ($dischi as $card) { ?>
+                    <!-- ciclo foreach per stampare i dati delle card -->
+                    <?php foreach ($card as $datiCard) { ?>         
+                        <?php 
+                            echo $datiCard;
+                        ?>
+                    <?php } ?>
+                    <!-- /ciclo foreach per stampare i dati delle card -->
                 <?php } ?>
-                <!-- /ciclo foreach per stampare i dati delle card -->
-            <?php } ?>
-            <!-- /ciclo foreach per stampare le card -->
+                <!-- /ciclo foreach per stampare le card -->
+            </div> 
         </main>
         <!-- /main -->
     </body>
