269f2330c519c616df5d2fdbcd098d024575cb37
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-02-02 19:04:08 +0100

    Creata la navbar
    Creata la navbar

--- milestone-1/index.php
+++ milestone-1/index.php
@@ -65,12 +65,13 @@ foreach ($dischi as $card) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Php-ajax-dischi</title>
+        <link rel="stylesheet" href="../dist/css/app.css">
     </head>
     <body>
         <!-- header -->
         <header>
             <nav>
-                <!-- <img src="../dist/img/logo.png" alt="logo di spotify"> -->
+                 <img src="../dist/img/logo.png" alt="logo di spotify">
             </nav>
         </header>
         <!-- /header -->
